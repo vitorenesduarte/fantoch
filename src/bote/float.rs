8e0e74ac82ae1b8ad49f8223219664591dd9e6de
Vitor Enes <vitorenesduarte@gmail.com>
2019-11-27 11:40:51 +0000

    add float tests

--- src/bote/float.rs
+++ src/bote/float.rs
@@ -14,6 +14,10 @@ impl F64 {
         Self::new(0.0)
     }
 
+    pub fn nan() -> Self {
+        Self::new(std::f64::NAN)
+    }
+
     /// Rounds the inner `f64` with 1 decimal place.
     pub fn round(&self) -> String {
         format!("{:.1}", self.0)
@@ -79,5 +83,81 @@ impl Eq for F64 {}
 impl fmt::Debug for F64 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new_with_zero() {
+        assert_eq!(F64::new(0.0), F64::zero());
+    }
+
+    #[test]
+    fn value() {
+        assert_eq!(F64::zero().value(), 0.0);
+        assert_eq!(F64::new(11.2).value(), 11.2);
+    }
+
+    #[test]
+    fn add() {
+        use std::ops::Add;
+
+        let x = F64::new(11.2);
+        let y = F64::new(30.8);
+        let expected = F64::new(42.0);
+
+        let result = x.add(y);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn add_assign() {
+        use std::ops::AddAssign;
+
+        let mut x = F64::new(11.2);
+        let y = F64::new(30.8);
+        let expected = F64::new(42.0);
+
+        x.add_assign(y);
+        assert_eq!(x, expected);
+    }
+
+    #[test]
+    fn sub() {
+        use std::ops::Sub;
+
+        let x = F64::new(10.8);
+        let y = F64::new(30.8);
+        let expected = F64::new(-20.0);
+
+        let result = x.sub(y);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn mul() {
+        use std::ops::Mul;
+
+        let x = F64::new(5.2);
+        let y = F64::new(5.3);
+        let expected = F64::new(27.56);
+
+        let result = x.mul(y);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn ord() {
+        use std::cmp::Ordering;
+        use std::f64::NAN;
+        assert_eq!(F64::new(5.2).cmp(&F64::new(5.3)), Ordering::Less);
+        assert_eq!(F64::new(5.3).cmp(&F64::new(5.2)), Ordering::Greater);
+        assert_eq!(F64::new(5.2).cmp(&F64::new(5.2)), Ordering::Equal);
+        assert_eq!(F64::new(5.2).cmp(&F64::nan()), Ordering::Less);
+        assert_eq!(F64::nan().cmp(&F64::new(5.3)), Ordering::Greater);
+        assert_eq!(F64::nan().cmp(&F64::nan()), Ordering::Equal);
     }
 }
