96955b9ca5be08750944d3fb5009c3d732b9214f
Vitor Enes <vitorenesduarte@gmail.com>
2019-12-09 22:44:41 +0000

    add workload test

--- src/client/workload.rs
+++ src/client/workload.rs
@@ -24,14 +24,11 @@ impl Workload {
 
     /// Generate the next command.
     pub fn next_cmd(&mut self, rifl_gen: &mut RiflGen) -> Option<Command> {
-        // increment command count and generate command
-        self.command_count += 1;
-
         // check if we should generate more commands
-        let should_gen = self.command_count <= self.total_commands;
-
-        // if we should, generate a new command
-        if should_gen {
+        if self.command_count < self.total_commands {
+            // increment command count
+            self.command_count += 1;
+            // generate new command
             Some(self.gen_cmd(rifl_gen))
         } else {
             None
@@ -53,12 +50,72 @@ impl Workload {
 
     /// Generate a command given
     fn gen_cmd_key(&mut self, rifl_gen: &RiflGen) -> Key {
-        if rand::thread_rng().gen_range(0, 100) < self.conflict_rate {
+        // check if we should generate a conflict
+        let should_conflict = match rand::thread_rng().gen_range(0, 100) {
+            0 => false,
+            n => n < self.conflict_rate,
+        };
+        if should_conflict {
             // black color to generate a conflict
             String::from("black")
         } else {
             // avoid conflict with unique client key
             rifl_gen.source().to_string()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gen_cmd_key() {
+        // create rilf gen
+        let client_id = 1;
+        let rifl_gen = RiflGen::new(client_id);
+
+        // total commands
+        let total_commands = 100;
+
+        // create conflicting workload
+        let conflict_rate = 100;
+        let mut workload = Workload::new(conflict_rate, total_commands);
+        assert_eq!(workload.gen_cmd_key(&rifl_gen), String::from("black"));
+
+        // create non-conflicting workload
+        let conflict_rate = 0;
+        let mut workload = Workload::new(conflict_rate, total_commands);
+        assert_eq!(workload.gen_cmd_key(&rifl_gen), String::from("1"));
+    }
+
+    #[test]
+    fn next_cmd() {
+        // create rilf gen
+        let client_id = 1;
+        let mut rifl_gen = RiflGen::new(client_id);
+
+        // total commands
+        let total_commands = 10;
+
+        // create workload
+        let conflict_rate = 100;
+        let mut workload = Workload::new(conflict_rate, total_commands);
+
+        // the first 10 commands are `Some`
+        for _ in 1..=10 {
+            assert!(workload.next_cmd(&mut rifl_gen).is_some());
+        }
+
+        // at this point, the number of commands generated equals `total_commands`
+        assert_eq!(workload.command_count, total_commands);
+
+        // after this, no more commands are generated
+        for _ in 1..=10 {
+            assert!(workload.next_cmd(&mut rifl_gen).is_none());
+        }
+
+        // at the number of commands generated is still `total_commands`
+        assert_eq!(workload.command_count, total_commands);
     }
 }
