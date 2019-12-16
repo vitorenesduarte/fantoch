f345fbd7e0be260ca222c16a39df0651d27da813
Vitor Enes <vitorenesduarte@gmail.com>
2019-12-16 12:19:17 +0000

    don't pre-compute percentiles

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -18,9 +18,7 @@ pub struct Stats {
     mean: F64,
     cov: F64,  // coefficient of variation
     mdtm: F64, // mean distance to mean
-    p5: F64,
-    p95: F64,
-    p99: F64,
+    values: Vec<u64>,
 }
 
 impl fmt::Debug for Stats {
@@ -30,16 +28,16 @@ impl fmt::Debug for Stats {
 }
 
 impl Stats {
-    pub fn from(latencies: Vec<u64>) -> Self {
-        let (mean, cov, mdtm) = Stats::compute_stats(&latencies);
-        let (p5, p95, p99) = Stats::compute_percentiles(latencies);
+    pub fn from(mut values: Vec<u64>) -> Self {
+        // sort all values (so that percentiles can be computed)
+        values.sort_unstable();
+        // compute some stats
+        let (mean, cov, mdtm) = Stats::compute_stats(&values);
         Self {
             mean,
             cov,
             mdtm,
-            p5,
-            p95,
-            p99,
+            values,
         }
     }
 
@@ -79,52 +77,38 @@ impl Stats {
         self.mdtm().round()
     }
 
-    fn compute_stats(xs: &[u64]) -> (F64, F64, F64) {
-        // transform `usize`s in `f64`s
-        let xs: Vec<f64> = xs.iter().map(|&x| x as f64).collect();
+    fn compute_stats(values: &[u64]) -> (F64, F64, F64) {
+        // transform `u64`s in `f64`s
+        let values: Vec<f64> = values.into_iter().map(|x| *x as f64).collect();
 
         // compute mean
-        let mean = Self::compute_mean(&xs);
+        let mean = Self::compute_mean(&values);
 
         // compute coefficient of variation
-        let cov = Self::compute_cov(&xs, mean);
+        let cov = Self::compute_cov(&values, mean);
 
         // compute mean distance to mean
-        let mdtm = Self::compute_mdtm(&xs, mean);
+        let mdtm = Self::compute_mdtm(&values, mean);
 
         // return the 3 stats
         (F64::new(mean), F64::new(cov), F64::new(mdtm))
     }
 
-    fn compute_percentiles(mut latencies: Vec<u64>) -> (F64, F64, F64) {
-        // first sort latencies
-        latencies.sort_unstable();
-        let p5 = Self::compute_percentile(0.5, &latencies);
-        let p95 = Self::compute_percentile(0.95, &latencies);
-        let p99 = Self::compute_percentile(0.99, &latencies);
-        (
-            F64::new(p5 as f64),
-            F64::new(p95 as f64),
-            F64::new(p99 as f64),
-        )
-    }
-
     // from https://rust-lang-nursery.github.io/rust-cookbook/science/mathematics/statistics.html
-    fn compute_mean(data: &[f64]) -> f64 {
-        let sum = data.iter().sum::<f64>();
-        let count = data.len() as f64;
-        // assumes `count > 0`
+    fn compute_mean(values: &[f64]) -> f64 {
+        let sum = values.iter().sum::<f64>();
+        let count = values.len() as f64;
         sum / count
     }
 
-    fn compute_cov(data: &[f64], mean: f64) -> f64 {
-        let stddev = Self::compute_stddev(data, mean);
+    fn compute_cov(values: &[f64], mean: f64) -> f64 {
+        let stddev = Self::compute_stddev(values, mean);
         stddev / mean
     }
 
-    fn compute_mdtm(data: &[f64], mean: f64) -> f64 {
-        let count = data.len() as f64;
-        let distances_sum = data
+    fn compute_mdtm(values: &[f64], mean: f64) -> f64 {
+        let count = values.len() as f64;
+        let distances_sum = values
             .iter()
             .map(|x| {
                 let diff = mean - x;
@@ -134,14 +118,14 @@ impl Stats {
         distances_sum / count
     }
 
-    fn compute_stddev(data: &[f64], mean: f64) -> f64 {
-        let variance = Self::compute_variance(data, mean);
+    fn compute_stddev(values: &[f64], mean: f64) -> f64 {
+        let variance = Self::compute_variance(values, mean);
         variance.sqrt()
     }
 
-    fn compute_variance(data: &[f64], mean: f64) -> f64 {
-        let count = data.len() as f64;
-        let sum = data
+    fn compute_variance(values: &[f64], mean: f64) -> f64 {
+        let count = values.len() as f64;
+        let sum = values
             .iter()
             .map(|x| {
                 let diff = mean - x;
@@ -153,19 +137,19 @@ impl Stats {
         sum / (count - 1.0)
     }
 
-    // Assumes `data` is sorted.
-    fn compute_percentile(percentile: f64, data: &[u64]) -> f64 {
+    // Assumes `self.values` is sorted.
+    pub fn percentile(&self, percentile: f64) -> F64 {
         assert!(percentile >= 0.0 && percentile <= 1.0);
-        let index = percentile * (data.len() as f64);
+        let index = percentile * (self.values.len() as f64);
         let index_rounded = index.round();
         // check if index is a whole number
         let is_whole_number = (index - index_rounded).abs() == 0.0;
 
         // compute final index and create data iterator
         let index = index_rounded as usize;
-        let mut data = data.into_iter();
+        let mut data = self.values.iter();
 
-        if is_whole_number {
+        let value = if is_whole_number {
             let left_value = data
                 .nth(index - 1)
                 .expect("there should be a left percentile value");
@@ -176,7 +160,9 @@ impl Stats {
                 .nth(index - 1)
                 .expect("there should a percentile value");
             *value as f64
-        }
+        };
+
+        F64::new(value)
     }
 }
 
@@ -239,13 +225,14 @@ mod tests {
     }
 
     #[test]
-    fn percentiles() {
+    fn percentile() {
         let data = vec![
             43, 54, 56, 61, 62, 66, 68, 69, 69, 70, 71, 72, 77, 78, 79, 85, 87, 88, 89, 93, 95, 96,
             98, 99, 99,
         ];
+        let stats = Stats::from(data);
 
-        assert_eq!(Stats::compute_percentile(0.9, &data), 98 as f64);
-        assert_eq!(Stats::compute_percentile(0.5, &data), 77 as f64);
+        assert_eq!(stats.percentile(0.9), F64::new(98.0));
+        assert_eq!(stats.percentile(0.5), F64::new(77.0));
     }
 }
