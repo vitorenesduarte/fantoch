1c7f0b074dc69d03bcefbeb210d76eb3750bdcde
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-07 20:44:55 +0000

    optimize away

--- src/map_merge.rs
+++ src/map_merge.rs
@@ -1,101 +1,66 @@
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
-use std::mem;
 
-// TODO maybe this can be implemented more efficiently once API's like `BTreeMap.first_entry` are
-// stabilized.
-pub fn merge<K, V, F>(map: &mut BTreeMap<K, V>, mut other: BTreeMap<K, V>, merge_fun: F)
+pub fn merge<K, V, F>(map: &mut BTreeMap<K, V>, other: BTreeMap<K, V>, merge_fun: F)
 where
     K: Ord + PartialEq + Eq,
-    K: std::fmt::Debug,
-    V: std::fmt::Debug,
     F: Fn(&mut V, V),
 {
     // create a peekable iterator of `map`
     let mut map_iter = map.iter_mut().peekable();
 
-    let smaller = if let Some((first_key, _)) = map_iter.peek() {
-        // remove all keys from `other` that are smaller than the first key in `map`
-        let mut remaining = other.split_off(first_key);
-        mem::swap(&mut remaining, &mut other);
-        remaining
-    } else {
-        // in this case, `map` is empty and we should simply replace `map` by `other`
-        mem::swap(map, &mut other);
-        return;
-    };
-    println!("other {:?} | smaller {:?}", other, smaller);
-    // at this point, the first key of `other` is at least a large as the first key of `map`
-
-    // create a peekable iterator of `other`
+    // create an iterator of `other`
     let mut other_iter = other.into_iter();
 
-    // hack to get around mutable usage in loop limitation as of rust 1.40 (as seen in `rio`)
-    let mut mc_holder = Some(map_iter.next());
-    let mut oc_holder = Some(other_iter.next());
+    // variables to hold the current value of each iterator
+    let mut map_current = map_iter.next();
+    let mut other_current = other_iter.next();
 
     // create vec where we'll store all entries that can't be inserted when interating `map`
     let mut extras = Vec::new();
 
     loop {
-        // get current pointers from their holders
-        let map_current = mc_holder.unwrap();
-        let other_current = oc_holder.unwrap();
-
-        println!(
-            "map current {:?} | other current {:?} | extras {:?}",
-            map_current, other_current, extras
-        );
-
-        let (map_current, other_current) = match (map_current, other_current) {
+        match (map_current, other_current) {
             (Some((map_key, map_value)), Some((other_key, other_value))) => {
                 match map_key.cmp(&other_key) {
                     Ordering::Equal => {
                         // merge values
                         merge_fun(map_value, other_value);
                         // advance both iterators
-                        (map_iter.next(), other_iter.next())
+                        map_current = map_iter.next();
+                        other_current = other_iter.next();
                     }
                     Ordering::Greater => {
-                        // save entry
-                        let entry = (other_key, other_value);
-                        extras.push(entry);
+                        // save entry to added later
+                        extras.push((other_key, other_value));
                         // only advance `other` iterator
-                        let map_current = (map_key, map_value);
-                        (Some(map_current), other_iter.next())
+                        map_current = Some((map_key, map_value));
+                        other_current = other_iter.next();
                     }
                     Ordering::Less => {
                         // only advance `map` iterator
-                        let other_current = (other_key, other_value);
-                        (map_iter.next(), Some(other_current))
+                        map_current = map_iter.next();
+                        other_current = Some((other_key, other_value));
                     }
                 }
             }
             (None, Some(entry)) => {
-                // the current key from `other` is the first key from `other` that is larger than
-                // the larger key in `map`:
-                // - save entry and break out of the loop
+                // the current key from in `entry` is the first key from `other` that is larger than
+                // the larger key in `map`; save entry and break out of the loop
                 extras.push(entry);
                 break;
             }
             (_, None) => {
-                // in the remaining cases (some, none) and (none, none) as we have incorporated all
-                // values from `other`
+                // there's nothing else to do here as in these (two) cases we have already
+                // incorporated all values from `other`
                 break;
             }
         };
-
-        // put back pointers
-        mc_holder = Some(map_current);
-        oc_holder = Some(other_current);
     }
 
-    // extend `map` with:
-    // - `smaller`: values from `other` that are smaller than the smaller key in `map`
-    // - `extras`: values from `other` that are larger than the smaller and smaller than the larger
-    //   but are not in `map`
-    // - `other`: values from `other` that are larger than the larger key in `map`
-    map.extend(smaller);
+    // extend `map` with values that are not in map:
+    // - `extras`: values from `other` that are smaller than the larger key in `map`
+    // - `other`:  values from `other` that are larger than the larger key in `map`
     map.extend(extras);
     map.extend(other_iter);
 }
@@ -103,6 +68,7 @@ where
 #[cfg(test)]
 mod proptests {
     use super::*;
+    use crate::elapsed;
     use quickcheck_macros::quickcheck;
 
     type Map = BTreeMap<u64, Vec<u64>>;
@@ -121,11 +87,9 @@ mod proptests {
 
     #[quickcheck]
     fn merge_check(mut map: Map, other: Map) -> bool {
-        println!("input: {:?} {:?}", map, other);
-        let expected = naive_merge(map.clone(), other.clone());
-        merge(&mut map, other, merge_fun);
-        println!("expected: {:?}", expected);
-        println!("result: {:?}", map);
+        let (naive_time, expected) = elapsed!(naive_merge(map.clone(), other.clone()));
+        let (time, ()) = elapsed!(merge(&mut map, other, merge_fun));
+        println!("{} {}", naive_time.as_nanos(), time.as_nanos());
         map == expected
     }
 }
