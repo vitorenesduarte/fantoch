f201ddbcd994d3a55176451d442e7abef7dd4251
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-07 20:54:17 +0000

    improve comments

--- src/map_merge.rs
+++ src/map_merge.rs
@@ -6,23 +6,34 @@ where
     K: Ord + PartialEq + Eq,
     F: Fn(&mut V, V),
 {
-    // create a peekable iterator of `map`
-    let mut map_iter = map.iter_mut().peekable();
-
-    // create an iterator of `other`
+    // create iterators for `map` and `other`
+    let mut map_iter = map.iter_mut();
     let mut other_iter = other.into_iter();
 
-    // variables to hold the current value of each iterator
+    // variables to hold the "current value" of each iterator
     let mut map_current = map_iter.next();
     let mut other_current = other_iter.next();
 
-    // create vec where we'll store all entries that can't be inserted when interating `map`
-    let mut extras = Vec::new();
+    // create vec where we'll store all entries with keys that are in `map`, are smaller than the
+    // larger key in `map`, but can't be inserted when interating `map`
+    let mut absent = Vec::new();
 
     loop {
         match (map_current, other_current) {
             (Some((map_key, map_value)), Some((other_key, other_value))) => {
                 match map_key.cmp(&other_key) {
+                    Ordering::Less => {
+                        // simply advance `map` iterator
+                        map_current = map_iter.next();
+                        other_current = Some((other_key, other_value));
+                    }
+                    Ordering::Greater => {
+                        // save entry to added later
+                        absent.push((other_key, other_value));
+                        // advance `other` iterator
+                        map_current = Some((map_key, map_value));
+                        other_current = other_iter.next();
+                    }
                     Ordering::Equal => {
                         // merge values
                         merge_fun(map_value, other_value);
@@ -30,38 +41,26 @@ where
                         map_current = map_iter.next();
                         other_current = other_iter.next();
                     }
-                    Ordering::Greater => {
-                        // save entry to added later
-                        extras.push((other_key, other_value));
-                        // only advance `other` iterator
-                        map_current = Some((map_key, map_value));
-                        other_current = other_iter.next();
-                    }
-                    Ordering::Less => {
-                        // only advance `map` iterator
-                        map_current = map_iter.next();
-                        other_current = Some((other_key, other_value));
-                    }
                 }
             }
             (None, Some(entry)) => {
-                // the current key from in `entry` is the first key from `other` that is larger than
-                // the larger key in `map`; save entry and break out of the loop
-                extras.push(entry);
+                // the key in `entry` is the first key from `other` that is larger than the larger
+                // key in `map`; save entry and break out of the loop
+                absent.push(entry);
                 break;
             }
             (_, None) => {
                 // there's nothing else to do here as in these (two) cases we have already
-                // incorporated all values from `other`
+                // incorporated all entries from `other`
                 break;
             }
         };
     }
 
-    // extend `map` with values that are not in map:
-    // - `extras`: values from `other` that are smaller than the larger key in `map`
-    // - `other`:  values from `other` that are larger than the larger key in `map`
-    map.extend(extras);
+    // extend `map` with keys from `other` that are not in `map`:
+    // - `absent`: keys from `other` that are smaller than the larger key in `map`
+    // - `other_iter`: keys from `other` that are larger than the larger key in `map`
+    map.extend(absent);
     map.extend(other_iter);
 }
 
