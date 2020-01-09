1ce7bc1a805efe3a4004ececedddb3d4df220241
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-09 13:30:55 +0000

    several binaries; default binary

---
+++ src/bin/run.rs
@@ -0,0 +1,10 @@
+use planet_sim::bote::Bote;
+use planet_sim::client::Workload;
+use planet_sim::config::Config;
+use planet_sim::metrics::Histogram;
+use planet_sim::planet::{Planet, Region};
+use planet_sim::protocol::{Atlas, EPaxos, Newt, Process};
+
+fn main() {
+    println!("run");
+}
