060ce14d19ee25c3fc439383ed54457c3bb2e0c9
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-11 18:21:43 +0000

    stream not needed for select

--- src/run/net/connection.rs
+++ src/run/net/connection.rs
@@ -1,98 +1,79 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use futures::prelude::*;
-use pin_project::pin_project;
 use serde::{de::DeserializeOwned, Serialize};
-use std::marker::PhantomData;
-use std::pin::Pin;
-use std::task::{Context, Poll};
-use tokio::io::{BufReader, BufWriter};
-use tokio::net::tcp::{ReadHalf, WriteHalf};
+use tokio::io::BufStream;
 use tokio::net::TcpStream;
-use tokio::stream::Stream;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
-// tokio default (in 0.2.9) that may change
-const DEFAULT_BUF_SIZE: usize = 8 * 1024;
-
 /// Delimits frames using a length header.
-#[pin_project]
 #[derive(Debug)]
-pub struct Connection<V> {
-    #[pin]
-    stream: Framed<TcpStream, LengthDelimitedCodec>,
-    phantom: PhantomData<V>,
+pub struct Connection {
+    stream: Framed<BufStream<TcpStream>, LengthDelimitedCodec>,
 }
 
-impl<V> Connection<V> {
+#[allow(dead_code)]
+impl Connection {
     pub fn new(stream: TcpStream) -> Self {
+        // TODO here `BufStream` will allocate two buffers, one for reading and another one for
+        // writing; this may be unnecessarily inneficient for users that will only read or write; on
+        // the other end, the allocation only occurs once, so it's probably fine to do this
+        let stream = BufStream::new(stream);
         let stream = Framed::new(stream, LengthDelimitedCodec::new());
-        Self {
-            stream,
-            phantom: PhantomData,
-        }
+        Self { stream }
     }
-}
 
-impl<V> Connection<V>
-where
-    V: Serialize + DeserializeOwned,
-{
-    // TODO here we only need a reference to the message; can we optimize this and not have each
-    // connection own a copy of the message when sending it?
-    pub async fn send(&mut self, value: &V) {
-        // serialize
-        let bytes = bincode::serialize(value).expect("serialize should work");
-        // TODO do we need `Bytes`?
-        let bytes = Bytes::from(bytes);
+    // TODO here we only need a reference to the value
+    pub async fn send<V>(&mut self, value: V)
+    where
+        V: Serialize,
+    {
+        // serialize and send
+        let bytes = Self::serialize(&value);
         if let Err(e) = self.stream.send(bytes).await {
-            println!("error while writing to socket: {:?}", e);
+            println!("[connection] error while writing to socket: {:?}", e);
         }
     }
 
-    pub async fn recv(&mut self) -> Option<V> {
+    pub async fn recv<V>(&mut self) -> Option<V>
+    where
+        V: DeserializeOwned,
+    {
         match self.stream.next().await {
-            Some(result) => match result {
-                Ok(bytes) => {
-                    // if it is, and not an error, deserialize it
-                    let value =
-                        bincode::deserialize(&bytes).expect("[connection] deserialize should work");
-                    Some(value)
-                }
-                Err(e) => {
-                    println!("[connection] error while reading from socket: {:?}", e);
-                    None
-                }
-            },
+            Some(Ok(bytes)) => {
+                // if it is, and not an error, deserialize it
+                let value = Self::deserialize(bytes);
+                Some(value)
+            }
+            Some(Err(e)) => {
+                println!("[connection] error while reading from socket: {:?}", e);
+                None
+            }
             None => None,
         }
     }
+
+    fn serialize<V>(value: &V) -> Bytes
+    where
+        V: Serialize,
+    {
+        // TODO can we avoid `Bytes`?
+        let bytes = bincode::serialize(value).expect("[connection] serialize should work");
+        Bytes::from(bytes)
+    }
+
+    fn deserialize<V>(bytes: BytesMut) -> V
+    where
+        V: DeserializeOwned,
+    {
+        bincode::deserialize(&bytes).expect("[connection] deserialize should work")
+    }
 }
 
-impl<V> Stream for Connection<V>
-where
-    V: DeserializeOwned,
-{
-    type Item = V;
+#[cfg(test)]
+mod tests {
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        match self.project().stream.poll_next(cx) {
-            Poll::Ready(result) => match result {
-                // if the result is ready, check if it's something
-                Some(result) => match result {
-                    Ok(bytes) => {
-                        // if it is, and not an error, deserialize it
-                        let value = bincode::deserialize(&bytes)
-                            .expect("[connection] deserialize should work");
-                        Poll::Ready(Some(value))
-                    }
-                    Err(e) => {
-                        println!("[connection] error while reading from socket: {:?}", e);
-                        Poll::Pending
-                    }
-                },
-                None => Poll::Ready(None),
-            },
-            Poll::Pending => Poll::Pending,
-        }
+    #[tokio::test]
+    async fn send_and_recv() {
+        // create
     }
 }
