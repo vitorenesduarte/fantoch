592c1f9b3508aff6f47d9cd161b595f2cfbbd176
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-14 21:49:37 +0000

    conflict rate 0 as default

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -4,7 +4,7 @@ use planet_sim::id::ClientId;
 use std::error::Error;
 
 const RANGE_SEP: &str = "-";
-const DEFAULT_CONFLICT_RATE: usize = 100;
+const DEFAULT_CONFLICT_RATE: usize = 0;
 const DEFAULT_COMMANDS_PER_CLIENT: usize = 1000;
 
 #[tokio::main]
