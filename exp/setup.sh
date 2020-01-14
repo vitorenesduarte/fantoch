a8f4bdf71bd69aed5af20142a2f6e30326972028
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-14 15:34:04 +0000

    setup script

---
+++ exp/setup.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+# shellcheck source=util.sh
+source "${DIR}/util.sh"
+
+run_build() {
+    # variables
+    local branch
+
+    # get branch
+    branch=$(config branch)
+
+    # build deps in each machines
+    while IFS= read -r machine; do
+        # copy build file to machine
+        scp "${SSH_ARGS}" "${BUILD_FILE}" "${machine}:build.sh"
+
+        # execute build in machine
+        # shellcheck disable=SC2029
+        ssh "${SSH_ARGS}" "${machine}" "./build.sh ${branch}" </dev/null &
+    done <"${MACHINES_FILE}"
+
+    # wait for all builds to complete
+    wait_jobs
+
+    info "all builds have been completed"
+}
+
+fetch_ips() {
+    # variables
+    local ip
+
+    # drop current ips
+    rm -f "${MACHINE_IPS_FILE}"
+
+    # fetch ip from each machine
+    while IFS= read -r machine; do
+
+        # get machine ip
+        # shellcheck disable=SC2029
+        ip=$(ssh "${SSH_ARGS}" "${machine}" hostname -I </dev/null | awk '{ print $1 }')
+
+        echo "${machine} ${ip}" >>"${MACHINE_IPS_FILE}"
+    done <"${MACHINES_FILE}"
+
+    info "all ips have been fetched"
+    cat "${MACHINE_IPS_FILE}"
+}
+
+run_build
+fetch_ips
