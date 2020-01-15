b7054a13804f23b1d0e67cc0d9ac7c9f988ee1c0
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-15 13:30:50 +0000

    remove dot upon commit in atlas

--- src/protocol/atlas.rs
+++ src/protocol/atlas.rs
@@ -282,6 +282,10 @@ impl Atlas {
             self.to_executor.push(execution_info);
         }
 
+        // TODO the following is incorrect: it should only be deleted once it has been committed at
+        // all processes
+        self.cmds.remove(dot);
+
         // nothing to send
         None
     }
