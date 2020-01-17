b54828bbe29eae0311884123c1ae82b66c865a8e
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-17 16:01:37 +0000

    increase maximum number of open files

--- exp/files/build.sh
+++ exp/files/build.sh
@@ -5,10 +5,10 @@ RUST_NIGHTLY="true"
 # - needed for RUN_MODE="flamegraph"
 FLAMEGRAPH="true"
 # flag indicating whether we should just remove planet_sim folder
-NUKE_PLANET_SIM="false"
+NUKE_PLANET_SIM="true"
 
 # maximum number of open files
-MAX_OPEN_FILES=10000
+MAX_OPEN_FILES=100000
 
 if [ $# -ne 1 ]; then
     echo "usage: build.sh branch"
@@ -43,19 +43,21 @@ if [ "${FLAMEGRAPH}" == "true" ]; then
     # - then append correct setting
     echo "kernel.perf_event_paranoid = -1" | sudo tee -a /etc/sysctl.conf
 
+    # reload system configuration so that previous changes  take place
+    sudo sysctl --system
+
     # install flamegraph
     sudo apt-get install -y linux-tools-common linux-tools-generic
     cargo install flamegraph
 fi
 
-# increase maximum number of open files by changing "sys.fs.file-max"
+# increase maximum number of open files by changing "/etc/security/limits.conf"
 # - first delete current setting, if any
-sudo sed -i '/^sys.fs.file-max.*/d' /etc/sysctl.conf
+sudo sed -i '/.*soft.*nofile.*/d' /etc/security/limits.conf
+sudo sed -i '/.*hard.*nofile.*/d' /etc/security/limits.conf
 # - then append correct setting
-echo "sys.fs.file-max = ${MAX_OPEN_FILES}" | sudo tee -a /etc/sysctl.conf
-
-# reload system configuration so that previous changes  take place
-sudo sysctl --system
+echo "*                soft    nofile          ${MAX_OPEN_FILES}" | sudo tee -a /etc/security/limits.conf
+echo "*                hard    nofile          ${MAX_OPEN_FILES}" | sudo tee -a /etc/security/limits.conf
 
 # install dstat
 sudo apt-get install -y dstat
