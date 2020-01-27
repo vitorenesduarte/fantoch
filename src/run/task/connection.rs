068dbae241209e08d1cb0c41a59d1a680d770e91
Vitor Enes <vitorenesduarte@gmail.com>
2020-01-27 21:53:17 +0000

    remove buffering in TCP stack

--- src/run/task/connection.rs
+++ src/run/task/connection.rs
@@ -16,7 +16,7 @@ impl Connection {
     // TODO here `BufStream` will allocate two buffers, one for reading and another one for
     // writing; this may be unnecessarily inneficient for users that will only read or write; on
     // the other end, the allocation only occurs once, so it's probably fine to do this
-    pub fn new(mut stream: TcpStream, tcp_nodelay: bool, socket_buffer_size: usize) -> Self {
+    pub fn new(stream: TcpStream, tcp_nodelay: bool, socket_buffer_size: usize) -> Self {
         println!("SO_RCVBUF: {:?}", stream.recv_buffer_size());
         println!("SO_SNDBUF: {:?}", stream.send_buffer_size());
         stream
