3d97088d55cd18de257abf3aa190b91ff1e2c0c7
Vitor Enes <vitorenesduarte@gmail.com>
2020-02-03 11:39:12 +0000

    add atomic_newt binary

---
+++ src/bin/atomic_newt.rs
@@ -0,0 +1,38 @@
+mod common;
+
+use planet_sim::protocol::{Protocol, AtomicNewt};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let (
+        process_id,
+        sorted_processes,
+        ip,
+        port,
+        client_port,
+        addresses,
+        config,
+        tcp_nodelay,
+        tcp_buffer_size,
+        tcp_flush_interval,
+        channel_buffer_size,
+        multiplexing,
+    ) = common::protocol::parse_args();
+    let process = AtomicNewt::new(process_id, config);
+
+    common::tokio_runtime().block_on(planet_sim::run::process(
+        process,
+        process_id,
+        sorted_processes,
+        ip,
+        port,
+        client_port,
+        addresses,
+        config,
+        tcp_nodelay,
+        tcp_buffer_size,
+        tcp_flush_interval,
+        channel_buffer_size,
+        multiplexing,
+    ))
+}
