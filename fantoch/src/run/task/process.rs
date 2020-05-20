8d98e0ceb7d646d0f63fa6352b169e6a406141e8
Vitor Enes <vitorenesduarte@gmail.com>
2020-05-20 10:41:08 +0100

    more work?

--- fantoch/src/run/task/process.rs
+++ fantoch/src/run/task/process.rs
@@ -366,12 +366,12 @@ async fn process_task<P>(
                 msg = from_readers.recv().fuse() => {
                     selected_from_processes(worker_index, process_id, msg, &mut process, &mut to_writers, &mut reader_to_workers, &mut worker_to_executors, &mut to_execution_logger).await
                 }
+                event = from_periodic.recv().fuse() => {
+                    selected_from_periodic_task(worker_index, process_id, event, &mut process, &mut to_writers, &mut reader_to_workers).await
+                }
                 cmd = from_clients.recv().fuse()  => {
                     selected_from_client(worker_index, process_id, cmd, &mut process, &mut to_writers, &mut reader_to_workers).await
                 }
-                // event = from_periodic.recv().fuse() => {
-                //     selected_from_periodic_task(worker_index, process_id, event, &mut process, &mut to_writers, &mut reader_to_workers).await
-                // }
             }
         }
     } else {
@@ -380,12 +380,12 @@ async fn process_task<P>(
                 msg = from_readers.recv() => {
                     selected_from_processes(worker_index, process_id, msg, &mut process, &mut to_writers, &mut reader_to_workers, &mut worker_to_executors, &mut to_execution_logger).await
                 }
+                event = from_periodic.recv() => {
+                    selected_from_periodic_task(worker_index, process_id, event, &mut process, &mut to_writers, &mut reader_to_workers).await
+                }
                 cmd = from_clients.recv() => {
                     selected_from_client(worker_index, process_id, cmd, &mut process, &mut to_writers, &mut reader_to_workers).await
                 }
-                // event = from_periodic.recv() => {
-                //     selected_from_periodic_task(worker_index, process_id, event, &mut process, &mut to_writers, &mut reader_to_workers).await
-                // }
             }
         }
     }
