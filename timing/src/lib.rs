8c56b89288df7b00fa3793ed1759d97b01a7d1f3
Vitor Enes <vitorenesduarte@gmail.com>
2020-05-21 13:23:37 +0100

    add timing crate

---
+++ timing/src/lib.rs
@@ -0,0 +1,116 @@
+use dashmap::DashMap;
+use hdrhistogram::Histogram;
+use quanta::Clock;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicU64, Ordering};
+use tracing::event::Event;
+use tracing::span::{Attributes, Id, Record};
+use tracing::{Metadata, Subscriber};
+
+thread_local! {
+    static CLOCK: Clock = Clock::new();
+    static START_TIMES: RefCell<HashMap<u64, u64>> = RefCell::new(HashMap::new());
+}
+
+// assume 1 second as the highest execution time
+const MAX_FUNCTION_EXECUTION_TIME: u64 = 1_000_0000;
+
+/// Compute current time.
+fn now() -> u64 {
+    CLOCK.with(|clock| clock.now().as_u64())
+}
+
+/// Record function start time.
+fn start(id: u64, time: u64) {
+    START_TIMES.with(|start_times| {
+        let res = start_times.borrow_mut().insert(id, time);
+        assert!(res.is_none());
+    })
+}
+
+/// Retrieve function start time.
+fn end(id: u64) -> u64 {
+    START_TIMES.with(|start_times| {
+        start_times
+            .borrow_mut()
+            .remove(&id)
+            .expect("function should have been started")
+    })
+}
+
+pub struct TimingSubscriber {
+    last_id: AtomicU64,
+    // mapping from function name to id used for that function
+    functions: DashMap<&'static str, u64>,
+    // mapping from function name to its histogram
+    histograms: DashMap<u64, Histogram<u64>>,
+}
+
+impl TimingSubscriber {
+    pub fn new() -> Self {
+        Self {
+            last_id: AtomicU64::new(0),
+            functions: DashMap::new(),
+            histograms: DashMap::new(),
+        }
+    }
+}
+
+impl Subscriber for TimingSubscriber {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn new_span(&self, span: &Attributes) -> Id {
+        // compute function name
+        let function_name = span.metadata().name();
+
+        // get function id
+        let id = match self.functions.get(function_name) {
+            Some(id) => {
+                // if the `function_name` already has an id associated, use it
+                *id.value()
+            }
+            None => {
+                // if here, it means that when `self.functions.get` was
+                // executed, `function_name` had no id associated; let's try to
+                // create one (making sure that no two threads create different
+                // ids for the same function name)
+                *self.functions.entry(function_name).or_insert_with(|| {
+                    self.last_id.fetch_add(1, Ordering::SeqCst)
+                })
+            }
+        };
+        Id::from_u64(id)
+    }
+
+    fn record(&self, _span: &Id, _values: &Record) {}
+
+    fn record_follows_from(&self, _span: &Id, _follows: &Id) {}
+
+    fn event(&self, _event: &Event) {}
+
+    fn enter(&self, span: &Id) {
+        let id = span.into_u64();
+        let start_time = now();
+        start(id, start_time);
+    }
+
+    fn exit(&self, span: &Id) {
+        let id = span.into_u64();
+        let end_time = now();
+        let start_time = end(id);
+        // function execution time in nanos
+        let time = end_time - start_time;
+        // retrieve histogram
+        let mut histogram = self.histograms.entry(id).or_insert_with(|| {
+            Histogram::<u64>::new_with_bounds(1, MAX_FUNCTION_EXECUTION_TIME, 3)
+                .expect("creating histogram should work")
+        });
+        // and update it
+        histogram
+            .record(time)
+            .expect("adding to histogram should work");
+    }
+}
