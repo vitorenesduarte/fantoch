2010bc342f9e98c4b43acc0773dca5d99b46f5da
Vitor Enes <vitorenesduarte@gmail.com>
2020-06-03 18:34:45 +0200

    ice

--- fantoch_exp/src/main.rs
+++ fantoch_exp/src/main.rs
@@ -1,13 +1,16 @@
 use color_eyre::Report;
 use rusoto_core::Region;
+use std::collections::HashMap;
+use std::fs::File;
 use std::time::Duration;
 use tracing::instrument;
 use tsunami::providers::aws;
 use tsunami::{Machine, Tsunami};
 
-const INSTANCE_TYPE: &str = "t3.small";
+const INSTANCE_TYPE: &str = "t3.medium";
 const MAX_SPOT_INSTANCE_REQUEST_WAIT: u64 = 120; // seconds
 const MAX_INSTANCE_DURATION: usize = 1; // hours
+const EXPERIMENT_DURATION: usize = 60; // seconds
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
@@ -20,7 +23,7 @@ async fn main() -> Result<(), Report> {
         // Region::AfSouth1,
         Region::ApEast1,
         Region::ApNortheast1,
-        Region::ApNortheast2,
+        // Region::ApNortheast2, special-region
         Region::ApSouth1,
         Region::ApSoutheast1,
         Region::ApSoutheast2,
@@ -38,6 +41,8 @@ async fn main() -> Result<(), Report> {
         Region::UsWest1,
         Region::UsWest2,
     ];
+
+    let all_regions = vec![Region::EuWest2, Region::UsWest2];
 
     let mut descriptors = Vec::with_capacity(all_regions.len());
     for region in &all_regions {
@@ -63,8 +68,12 @@ async fn main() -> Result<(), Report> {
         descriptors.push((name, setup))
     }
 
+    println!("--> descriptors {:?}", descriptors);
+
     let mut launcher: tsunami::providers::aws::Launcher<_> = Default::default();
     launcher.set_max_instance_duration(MAX_INSTANCE_DURATION);
+    println!("--> launcher {:?}", launcher);
+
     launcher
         .spawn(
             descriptors,
@@ -72,6 +81,8 @@ async fn main() -> Result<(), Report> {
         )
         .await?;
     let vms = launcher.connect_all().await?;
+
+    println!("--> vms {:?}", vms);
 
     let pings = all_regions
         .clone()
@@ -83,8 +94,37 @@ async fn main() -> Result<(), Report> {
             ping(from, to)
         });
 
-    for result in futures::future::join_all(pings).await {
-        println!("{:?}", result);
+    let mut results = HashMap::new();
+    for (from, to, result) in futures::future::join_all(pings).await? {
+        // find the line of interest
+        let mut aggregate: Vec<_> = results
+            .lines()
+            .into_iter()
+            .filter(|line| line.contains("min/avg/max"))
+            .collect();
+        assert_eq!(aggregate.len(), 1);
+        let aggregate = aggregate.pop().unwrap();
+
+        println!("{} -> {}: {}", from.name(), to.name(), aggregate);
+
+        // create new ping stats
+        let stats = format!("{}:{}", aggregate, to.name());
+
+        // save stats
+        results
+            .entry(from.name())
+            .or_insert_with(Vec::new)
+            .push(stats);
+    }
+
+    for (region, mut stats) in results {
+        // sort ping results
+        stats.sort();
+        let content = stats.join("\n");
+
+        let file = File::create(format!("{}.dat", region.name()))
+            .expect("it should be possible to create dat file");
+        file.write(content);
     }
 
     launcher.terminate_all().await?;
@@ -92,18 +132,24 @@ async fn main() -> Result<(), Report> {
 }
 
 #[instrument]
-async fn ping(from: &Machine<'_>, to: &Machine<'_>) -> Result<(), Report> {
+async fn ping<'a>(
+    from: &'a Machine<'a>,
+    to: &'a Machine<'a>,
+) -> Result<(&'a Machine<'a>, &'a Machine<'a>, String), Report> {
     let to_ip = &to.public_ip;
 
     let out = from
         .ssh
         .command("ping")
-        .arg("-c")
-        .arg("10")
+        // specify the duration of the experiment
+        .arg("-w")
+        .arg(EXPERIMENT_DURATION.to_string())
+        // specify the ping timeout: never
+        .arg("-W")
+        .arg(0.to_string())
         .arg(&to_ip)
         .output()
         .await?;
-    let stdout = std::string::String::from_utf8(out.stdout)?;
-    tracing::info!(ping = %stdout, "ping");
-    Ok(())
+    let stdout = String::from_utf8(out.stdout)?;
+    Ok((from, to, stdout))
 }
