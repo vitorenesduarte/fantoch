328859ccf8603f6cfc5a8c6772cdb90da4c42681
Vitor Enes <vitorenesduarte@gmail.com>
2020-06-15 19:55:55 +0200

    single round of votes in bump_and_vote atomic clocks

--- fantoch_ps/src/protocol/common/table/clocks/keys/atomic.rs
+++ fantoch_ps/src/protocol/common/table/clocks/keys/atomic.rs
@@ -34,9 +34,11 @@ impl KeyClocks for AtomicKeyClocks {
     }
 
     fn bump_and_vote(&mut self, cmd: &Command, min_clock: u64) -> (u64, Votes) {
-        // first round of votes:
+        // single round of votes:
         // - vote on each key and compute the highest clock seen
-        let mut first_round_votes = Vec::with_capacity(cmd.key_count());
+        // - this means that if we have more than one key, then we don't
+        //   necessarily end up with all key clocks equal
+        let mut votes = Votes::with_capacity(cmd.key_count());
         let highest = cmd
             .keys()
             .map(|key| {
@@ -48,48 +50,13 @@ impl KeyClocks for AtomicKeyClocks {
 
                 // create vote range and save it
                 let vr = VoteRange::new(self.id, vote_start, vote_end);
-                first_round_votes.push((key.clone(), vr));
+                votes.set(key.clone(), vec![vr]);
 
                 // return vote end
                 vote_end
             })
             .max()
             .expect("there should be a maximum sequence");
-
-        // create votes
-        let mut votes = Votes::with_capacity(cmd.key_count());
-
-        // second round of votes:
-        // - vote on the keys that have a clock lower than the computed
-        //   `highest`
-        first_round_votes.into_iter().for_each(|(key, first_vr)| {
-            // check if we should vote more
-            if first_vr.end() < highest {
-                // try to bump up to `highest`
-                // - we really mean try because maybe votes by other threads
-                //   have been generated and it's no longer possible to generate
-                //   votes below `highest`
-                // - this means that this second round is actually optional is
-                //   it's only here for performance reasons (i.e. reduce
-                //   execution delay)
-                if let Some(previous_value) = self.maybe_bump(&key, highest) {
-                    // compute vote start and vote end
-                    let vote_start = previous_value + 1;
-                    let vote_end = highest;
-
-                    // create second vote range and save it
-                    let second_vr =
-                        VoteRange::new(self.id, vote_start, vote_end);
-                    // save the two votes on this key
-                    votes
-                        .set(key, VoteRange::try_compress(first_vr, second_vr));
-                    return;
-                }
-            }
-            // if we didn't vote agian, then simply save the single vote from
-            // the first round
-            votes.set(key, vec![first_vr]);
-        });
         (highest, votes)
     }
 
