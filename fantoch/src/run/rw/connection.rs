157e20bcfc47422ccb977bafa30a4a37e8a17c99
Vitor Enes <vitorenesduarte@gmail.com>
2020-06-16 09:29:35 +0200

    don't set OS read and write buffer

--- fantoch/src/run/rw/connection.rs
+++ fantoch/src/run/rw/connection.rs
@@ -36,26 +36,26 @@ fn configure(stream: &TcpStream, tcp_nodelay: bool, tcp_buffer_size: usize) {
         .set_nodelay(tcp_nodelay)
         .expect("setting TCP_NODELAY should work");
 
-    // maybe adapt SO_RCVBUF and SO_SNDBUF and compute buffer capacity
-    // change SO_RCVBUF if lower than `tcp_buffer_size`
-    if let Ok(so_rcvbuf) = stream.recv_buffer_size() {
-        if so_rcvbuf < tcp_buffer_size {
-            stream
-                .set_recv_buffer_size(tcp_buffer_size)
-                .expect("setting tcp recv buffer should work");
-        }
-    }
-    log!("SO_RCVBUF: {:?}", stream.recv_buffer_size());
+    // // maybe adapt SO_RCVBUF and SO_SNDBUF and compute buffer capacity
+    // // change SO_RCVBUF if lower than `tcp_buffer_size`
+    // if let Ok(so_rcvbuf) = stream.recv_buffer_size() {
+    //     if so_rcvbuf < tcp_buffer_size {
+    //         stream
+    //             .set_recv_buffer_size(tcp_buffer_size)
+    //             .expect("setting tcp recv buffer should work");
+    //     }
+    // }
+    // log!("SO_RCVBUF: {:?}", stream.recv_buffer_size());
 
-    // change SO_SNFBUF if lower than `tcp_buffer_size`
-    if let Ok(so_sndbuf) = stream.send_buffer_size() {
-        if so_sndbuf < tcp_buffer_size {
-            stream
-                .set_send_buffer_size(tcp_buffer_size)
-                .expect("setting tcp send buffer should work");
-        }
-    }
-    log!("SO_SNDBUF: {:?}", stream.send_buffer_size());
+    // // change SO_SNFBUF if lower than `tcp_buffer_size`
+    // if let Ok(so_sndbuf) = stream.send_buffer_size() {
+    //     if so_sndbuf < tcp_buffer_size {
+    //         stream
+    //             .set_send_buffer_size(tcp_buffer_size)
+    //             .expect("setting tcp send buffer should work");
+    //     }
+    // }
+    // log!("SO_SNDBUF: {:?}", stream.send_buffer_size());
 }
 
 impl Deref for Connection {
