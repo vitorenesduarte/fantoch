401992ee3b7d1ec4c422ce442206cec4c35f3c56
Vitor Enes <vitorenesduarte@gmail.com>
2020-06-20 17:47:55 +0200

    don't use DelayQueue in delay task

--- fantoch/src/run/task/delay.rs
+++ fantoch/src/run/task/delay.rs
@@ -1,5 +1,5 @@
 use super::chan::{ChannelReceiver, ChannelSender};
-use tokio::stream::StreamExt;
+use tokio::time::{self, Duration, Instant};
 
 async fn delay_task<M>(
     mut from: ChannelReceiver<M>,
@@ -8,30 +8,41 @@ async fn delay_task<M>(
 ) where
     M: std::fmt::Debug + 'static,
 {
-    let mut queue = tokio::time::DelayQueue::new();
-    let delay = tokio::time::Duration::from_millis(delay);
+    let mut queue = Vec::new();
+    let delay = Duration::from_millis(delay);
     loop {
-        tokio::select! {
-            msg = from.recv() => {
-                queue.insert(msg, delay);
+        match queue.first() {
+            None => {
+                let msg = from.recv().await;
+                enqueue(msg, delay, &mut queue);
             }
-            msg = queue.next() => {
-                if let Some(msg) = msg {
-                    match msg {
-                        Ok(msg) => {
-                            let msg = msg.into_inner().expect("[delay_task] there should be a msg");
-                            if let Err(e) = to.send(msg).await {
-                                println!("[delay_task] error forwarding message: {:?}", e);
-                            }
-                        }
-                        Err(e) => {
-                            println!("[delay_task] error taking message from queue: {:?}", e);
+            Some((next_instant, _)) => {
+                tokio::select! {
+                    _ = time::delay_until(*next_instant) => {
+                        let (_, msg) = queue.pop().unwrap();
+                        if let Err(e) = to.send(msg).await {
+                            println!("[delay_task] error forwarding message: {:?}", e);
                         }
                     }
-                } else {
-                    println!("[delay_task] nothing to take from the queue");
+                    msg = from.recv() => {
+                        enqueue(msg, delay, &mut queue);
+                    }
                 }
             }
         }
     }
+}
+
+fn enqueue<M>(msg: Option<M>, delay: Duration, queue: &mut Vec<(Instant, M)>) {
+    if let Some(msg) = msg {
+        queue.push((deadline(delay), msg));
+    } else {
+        println!("[delay_task] error receiving message from parent");
+    }
+}
+
+fn deadline(delay: Duration) -> Instant {
+    Instant::now()
+        .checked_add(delay)
+        .expect("deadline should exist")
 }
