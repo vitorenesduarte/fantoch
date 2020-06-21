14162d94e5137e9456017cc1329f674164bf2d1b
Vitor Enes <vitorenesduarte@gmail.com>
2020-06-21 18:59:24 +0200

    naming

--- fantoch/src/protocol/basic.rs
+++ fantoch/src/protocol/basic.rs
@@ -60,8 +60,8 @@ impl Protocol for Basic {
         };
 
         // create periodic events
-        let events = if let Some(gc_delay) = config.gc_interval() {
-            vec![(PeriodicEvent::GarbageCollection, gc_delay as u64)]
+        let events = if let Some(interval) = config.gc_interval() {
+            vec![(PeriodicEvent::GarbageCollection, interval as u64)]
         } else {
             vec![]
         };
