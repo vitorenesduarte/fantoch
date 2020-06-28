7eacb9c9cb8fc89687e4441a7bdfb79aa682c275
Vitor Enes <vitorenesduarte@gmail.com>
2020-06-28 14:07:59 +0200

    bump all deps

--- fantoch_plot/src/main.rs
+++ fantoch_plot/src/main.rs
@@ -10,17 +10,9 @@ fn main() -> PyResult<()> {
 
     let x = vec!["us-east-1", "ca-central-1", "eu-west-2"];
     let y = vec![10, 20, 30];
-    plt.subplot(2, 1, 1)?;
     plt.plot(x, y, "o-")?;
-    plt.title("A tale of 2 subplots")?;
+    plt.title("latency per region")?;
     plt.xlabel("regions")?;
-    plt.ylabel("latency (ms)")?;
-
-    let x = vec![8, 16, 32, 64, 128];
-    let y = vec![5, 10, 18, 32, 40];
-    plt.subplot(2, 1, 2)?;
-    plt.plot(x, y, ".-")?;
-    plt.xlabel("throughput (ops/s)")?;
     plt.ylabel("latency (ms)")?;
 
     let kwargs = &[("format", "pdf")];
