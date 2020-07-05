1c4c594ada9c0a99cf4d5023f67402ef5ff3b318
Vitor Enes <vitorenesduarte@gmail.com>
2020-07-05 10:59:27 +0200

    move pytry! inside plotting lib

--- fantoch_plot/src/plot/axis.rs
+++ fantoch_plot/src/plot/axis.rs
@@ -1,4 +1,7 @@
+use crate::pytry;
+use color_eyre::Report;
 use pyo3::prelude::*;
+use pyo3::PyNativeType;
 
 // https://matplotlib.org/api/axis_api.html?highlight=axis#matplotlib.axis.Axis
 pub struct Axis<'a> {
@@ -10,8 +13,12 @@ impl<'a> Axis<'a> {
         Self { axis }
     }
 
-    pub fn set_visible(&self, visible: bool) -> PyResult<()> {
-        self.axis.call_method1("set_visible", (visible,))?;
+    pub fn set_visible(&self, visible: bool) -> Result<(), Report> {
+        pytry!(self.py(), self.axis.call_method1("set_visible", (visible,)));
         Ok(())
+    }
+
+    fn py(&self) -> Python<'_> {
+        self.axis.py()
     }
 }
--- fantoch_plot/src/plot/figure.rs
+++ fantoch_plot/src/plot/figure.rs
@@ -1,5 +1,8 @@
+use crate::pytry;
+use color_eyre::Report;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
+use pyo3::PyNativeType;
 
 pub struct Figure<'a> {
     fig: &'a PyAny,
@@ -14,8 +17,18 @@ impl<'a> Figure<'a> {
         self.fig
     }
 
-    pub fn subplots_adjust(&self, kwargs: Option<&PyDict>) -> PyResult<()> {
-        self.fig.call_method("subplots_adjust", (), kwargs)?;
+    pub fn subplots_adjust(
+        &self,
+        kwargs: Option<&PyDict>,
+    ) -> Result<(), Report> {
+        pytry!(
+            self.py(),
+            self.fig.call_method("subplots_adjust", (), kwargs)
+        );
         Ok(())
+    }
+
+    fn py(&self) -> Python<'_> {
+        self.fig.py()
     }
 }
