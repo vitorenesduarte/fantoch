02339b8515ad1514a4d163a0d99182cb07dbddfd
Vitor Enes <vitorenesduarte@gmail.com>
2020-07-07 16:41:52 +0200

    refactor concurrent tests of key clocks

--- fantoch_ps/src/protocol/common/table/clocks/keys/atomic.rs
+++ fantoch_ps/src/protocol/common/table/clocks/keys/atomic.rs
@@ -48,7 +48,8 @@ impl KeyClocks for AtomicKeyClocks {
 
                 // create vote range and save it
                 let current_value = cmp::max(min_clock, previous_value + 1);
-                let vr = VoteRange::new(self.id, previous_value + 1, current_value);
+                let vr =
+                    VoteRange::new(self.id, previous_value + 1, current_value);
                 votes.set(key.clone(), vec![vr]);
 
                 // return "current" clock value
@@ -123,113 +124,5 @@ impl AtomicKeyClocks {
             let vr = VoteRange::new(id, previous_value + 1, up_to);
             votes.set(key.clone(), vec![vr]);
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::util;
-    use std::collections::BTreeSet;
-    use std::iter::FromIterator;
-    use std::thread;
-
-    #[test]
-    fn atomic_clocks_test() {
-        let nthreads = 2;
-        let ops_number = 10000;
-        let max_keys_per_command = 2;
-        let keys_number = 4;
-        for _ in 0..100 {
-            test(nthreads, ops_number, max_keys_per_command, keys_number);
-        }
-    }
-
-    fn test(
-        nthreads: usize,
-        ops_number: usize,
-        max_keys_per_command: usize,
-        keys_number: usize,
-    ) {
-        // create clocks
-        let process_id = 1;
-        let clocks = AtomicKeyClocks::new(process_id);
-
-        // spawn workers
-        let handles: Vec<_> = (0..nthreads)
-            .map(|_| {
-                let clocks_clone = clocks.clone();
-                thread::spawn(move || {
-                    worker(
-                        clocks_clone,
-                        ops_number,
-                        max_keys_per_command,
-                        keys_number,
-                    )
-                })
-            })
-            .collect();
-
-        // wait for all workers and aggregate their votes
-        let mut all_votes = Votes::new();
-        for handle in handles {
-            let votes = handle.join().expect("worker should finish");
-            all_votes.merge(votes);
-        }
-
-        // verify votes
-        for (_, key_votes) in all_votes {
-            // create set will all votes expanded
-            let mut expanded = BTreeSet::new();
-            for vote_range in key_votes {
-                for vote in vote_range.votes() {
-                    // insert vote and check it hasn't been added before
-                    expanded.insert(vote);
-                }
-            }
-
-            // check that we have all votes (i.e. we don't have gaps that would
-            // prevent timestamp-stability)
-            let vote_count = expanded.len();
-            // we should have all votes from 1 to `vote_cound`
-            assert_eq!(
-                expanded,
-                BTreeSet::from_iter((1..=vote_count).map(|vote| vote as u64))
-            );
-        }
-    }
-
-    fn worker(
-        mut clocks: AtomicKeyClocks,
-        ops_number: usize,
-        max_keys_per_command: usize,
-        keys_number: usize,
-    ) -> Votes {
-        // all votes worker has generated
-        let mut all_votes = Votes::new();
-
-        // highest clock seen
-        let mut highest = 0;
-
-        for _ in 0..ops_number {
-            // there are no noop's
-            let noop_probability = 0;
-            let cmd = util::gen_cmd(
-                max_keys_per_command,
-                keys_number,
-                noop_probability,
-            )
-            .expect(
-                "command shouldn't be a noop since the noop probability is 0",
-            );
-            // get votes
-            let (new_highest, votes) = clocks.bump_and_vote(&cmd, highest);
-            // update highest
-            highest = new_highest;
-            // save votes
-            all_votes.merge(votes);
-        }
-
-        all_votes
     }
 }
