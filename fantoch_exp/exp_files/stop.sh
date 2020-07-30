049918cf7b732fe2de79c0d0f3c3672fd50446c8
Vitor Enes <vitorenesduarte@gmail.com>
2020-07-30 21:43:46 +0200

    fantoch_exp: speed up stop.sh script

--- fantoch_exp/exp_files/stop.sh
+++ fantoch_exp/exp_files/stop.sh
@@ -5,9 +5,6 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 
 MACHINES_FILE="${DIR}/machines"
 SSH_ARGS="-oStrictHostKeyChecking=no"
-PORT=3000
-CLIENT_PORT=4000
-MAX_PROCS=24
 
 wait_jobs() {
     for job in $(jobs -p); do
@@ -16,19 +13,28 @@ wait_jobs() {
 }
 
 stop_fantoch() {
-    if [ $# -ne 1 ]; then
-        echo "usage: stop_fantoch machine"
+    if [ $# -ne 2 ]; then
+        echo "usage: stop_fantoch binary machine"
         exit 1
     fi
 
     # variables
-    local machine=$1
+    local binary=$1
+    local machine=$2
     local cmd
 
-    # stop processes
-    cmd="lsof -i :${PORT} -i :${CLIENT_PORT} | grep -v PID | awk '{ print \"kill -SIGKILL \"\$2 }' | sort -u | bash"
-    # shellcheck disable=SC2029
+    # stop process
+    cmd="pkill ${binary}"
     ssh "${SSH_ARGS}" ${machine} "${cmd}" </dev/null
+
+    # wait for process to end
+    cmd="ps -aux | grep ${binary} | grep -v grep | wc -l"
+    local running=-1
+    while [[ ${running} != 0 ]]; do
+        # shellcheck disable=SC2029
+        running=$(ssh "${SSH_ARGS}" ${machine} "${cmd}" </dev/null | xargs)
+        sleep 1
+    done
 
     # stop clients
     cmd="ps -aux | grep fantoch/target/release/client | grep -v grep | awk '{ print \"kill -SIGKILL \"\$2 }' | bash"
@@ -38,20 +44,6 @@ stop_fantoch() {
     cmd="ps -aux | grep dstat | grep -v grep | awk '{ print \"kill -SIGKILL \"\$2 }' | bash"
     ssh "${SSH_ARGS}" ${machine} "${cmd}" </dev/null
 
-    for process_id in $(seq 1 ${MAX_PROCS}); do
-        # compute ports
-        local port=$(( PORT + process_id ))
-        local client_port=$(( CLIENT_PORT + process_id ))
-
-        # wait for processes to end
-        cmd="lsof -i :${port} -i :${client_port} | wc -l"
-        local running=-1
-        while [[ ${running} != 0 ]]; do
-            # shellcheck disable=SC2029
-            running=$(ssh "${SSH_ARGS}" ${machine} "${cmd}" </dev/null | xargs)
-        done
-    done
-
     # remove files
     cmd="rm -f .metrics .log .metrics dstat.csv heaptrack.*.gz"
     ssh "${SSH_ARGS}" ${machine} "${cmd}" </dev/null
@@ -59,13 +51,13 @@ stop_fantoch() {
 
 stop_all() {
     # variables
+    local binary=$1
     local machine
 
     while IFS= read -r machine; do
-        stop_fantoch ${machine} &
+        stop_fantoch ${binary} ${machine} &
     done <"${MACHINES_FILE}"
     wait_jobs
 }
 
-stop_all
-
+stop_all $1
