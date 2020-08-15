7e701727b218973df061d6659646595cb04cf54f
Vitor Enes <vitorenesduarte@gmail.com>
2020-08-15 10:06:31 +0200

    GraphExecutor: add logs

--- fantoch_ps/src/executor/graph/mod.rs
+++ fantoch_ps/src/executor/graph/mod.rs
@@ -271,7 +271,9 @@ impl DependencyGraph {
                 from,
                 _time.millis()
             );
+            println!("@{} A", self.executor_index);
             if let Some(vertex) = self.vertex_index.find(&dot) {
+                println!("@{} B1", self.executor_index);
                 let vertex = vertex.read("Graph::process_requests");
 
                 // only send the vertex if the shard that requested this vertex
@@ -293,6 +295,7 @@ impl DependencyGraph {
                     })
                 }
             } else {
+                println!("@{} B2", self.executor_index);
                 // if we don't have it, then check if it's executed
                 // NOTE: this `executed_clock` is a snapshot of the clock, so we
                 // may endup buffering a request for the next cleanup step, even
