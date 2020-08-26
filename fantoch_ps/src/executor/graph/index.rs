f3814ea0fc94ab3633840d79eb6ee7457873f2b5
Vitor Enes <vitorenesduarte@gmail.com>
2020-08-26 16:20:08 +0200

    GraphExecutor: compute why commands are pending

--- fantoch_ps/src/executor/graph/index.rs
+++ fantoch_ps/src/executor/graph/index.rs
@@ -7,7 +7,7 @@ use fantoch::hash_map::{Entry, HashMap};
 use fantoch::id::{Dot, ProcessId, ShardId};
 use fantoch::time::SysTime;
 use fantoch::HashSet;
-use parking_lot::RwLock;
+use parking_lot::{RwLock, RwLockReadGuard};
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::time::Duration;
@@ -68,17 +68,30 @@ impl VertexIndex {
         let show_pending_threshold_ms =
             show_pending_threshold.as_millis() as u64;
         let mut pending = BTreeMap::new();
+        let mut pending_without_missing_deps = HashSet::new();
+
         self.index.iter().for_each(|vertex_ref| {
+            // check if we should show this pending command
             let vertex = vertex_ref.read();
             let pending_for_ms = now_ms - vertex.start_time_ms;
+
             if pending_for_ms >= show_pending_threshold_ms {
+                // compute missing dependencies
+                let mut visited = HashSet::new();
+                let missing_deps = self.missing_dependencies(&vertex, executed_clock, &mut visited);
+
+                if missing_deps.is_empty() {
+                    pending_without_missing_deps.insert(vertex.dot);
+                }
+
                 pending.entry(pending_for_ms).or_insert_with(Vec::new).push(
                     format!(
-                        "p{}: {:?} is pending for {:?}ms with deps {:?}",
+                        "p{}: {:?} is pending for {:?}ms with deps {:?} | missing {:?}",
                         self.process_id,
                         vertex.dot,
                         pending_for_ms,
-                        vertex.deps
+                        vertex.deps,
+                        missing_deps,
                     ),
                 )
             }
@@ -90,6 +103,50 @@ impl VertexIndex {
                 println!("{}", fmt);
             }
         }
+
+        // panic if there's a pending command without missing dependencies
+        if !pending_without_missing_deps.is_empty() {
+            panic!("p{}: the following commands are pending without missing dependencies: {:?}", self.process_id, pending_without_missing_deps);
+        }
+    }
+
+    fn missing_dependencies(
+        &self,
+        vertex: &RwLockReadGuard<'_, Vertex>,
+        executed_clock: &AEClock<ProcessId>,
+        visited: &mut HashSet<Dot>,
+    ) -> HashSet<Dot> {
+        let mut missing_dependencies = HashSet::new();
+
+        // add self to the set of visited pending commands
+        if !visited.insert(vertex.dot) {
+            // if already visited, return
+            return missing_dependencies;
+        }
+
+        for dep in &vertex.deps {
+            let dep_dot = dep.dot;
+            if executed_clock.contains(&dep_dot.source(), dep_dot.sequence()) {
+                // ignore executed dep
+                continue;
+            }
+
+            // if it's not executed, check if it's also pending
+            if let Some(dep_vertex_ref) = self.index.get(&dep_dot) {
+                let dep_vertex = dep_vertex_ref.read();
+                // if it is, then we're pending for the same reason our
+                // dependency is pending
+                missing_dependencies.extend(self.missing_dependencies(
+                    &dep_vertex,
+                    executed_clock,
+                    visited,
+                ));
+            } else {
+                // if it's not pending, then there's a missing dependency
+                missing_dependencies.insert(dep_dot);
+            }
+        }
+        missing_dependencies
     }
 }
 
